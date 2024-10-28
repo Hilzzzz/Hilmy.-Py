e80b64f0f5e46f9d0e6d3fdf5f4b6005caa790e8
Hilzzzz <azizhilmy68@gmail.com>
2024-10-28 17:01:45 +0700

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,7 @@
+from tkinter import*
+window = Tk()
+label = Label (window, text="Hello, ini adalah label di Tkinter!",font=("Arial",10))
+label.pack()
+label2 = Label(window, text = "Hallo,Hilmy XII Pplg 1",font=("Arial",10))
+label2.pack()
+window.mainloop()
